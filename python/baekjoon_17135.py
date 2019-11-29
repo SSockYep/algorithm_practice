cd31aa193c1f8a3a0389d41bb6c5fb92ca779b94
seol <95thomas811@gmail.com>
2019-11-29 17:13:56 +0900

    need more examples...

--- python/baekjoon_17135.py
+++ python/baekjoon_17135.py
@@ -1,8 +1,54 @@
-import fileinput
+# https://www.acmicpc.net/problem/17135
+import itertools
 
+global n, m, d, castle, count
+
+def shoot(turn, pos):
+    global castle, count, n, m, d
+    for i in range(0, d):
+        y = -turn
+        x = max(pos-i, 0)
+        while x <= min(pos+i, m-1):
+            if y < -n:
+                pass
+            elif y > -turn:
+                break
+            elif castle[y][x] == 1:
+                if not [y, x] in count:
+                    count.append([y, x])
+                return
+            if x < pos:
+                y -= 1
+            else:
+                y += 1
+            x += 1
+            
 def main():
-    fp = fileinput.input()
-    a = fp.readline()
+    global n, m, d, castle, count
+    n, m, d = input().split(' ')
+    n = int(n)
+    m = int(m)
+    d = int(d)
+    castle = []
+    count = []
+    res = 0
+    for i in range(n):
+        _ = list(map(int, input().split(' ')))
+        castle.append(_)
+
+    for c1, c2, c3 in itertools.combinations(range(m), 3):
+        for i in range(1, n+1):
+            shoot(i,c1)
+            shoot(i,c2)
+            shoot(i,c3)
+            for ss in count:
+                castle[ss[0]][ss[1]] = 0
+                
+        res = max(res, len(count))
+        for ss in count:
+            castle[ss[0]][ss[1]] = 1
+        count = []
+    print(res)
 
 if __name__ == '__main__':
     main()
